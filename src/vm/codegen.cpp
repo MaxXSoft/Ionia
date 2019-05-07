f6cb5789e5aa67231f26fbaeb7de5903a8b24197
MaxXSoft <x@MaxXSoft.net>
2019-05-07 19:45:24 +0800

    (codegen) added class 'VMCodeGen'

---
+++ src/vm/codegen.cpp
@@ -0,0 +1,62 @@
+#include "vm/codegen.h"
+
+#include <cstddef>
+
+#include "util/cast.h"
+
+int VMCodeGen::ParseBytecode(const std::vector<std::uint8_t> &buffer,
+                             VMSymbolTable &sym_table,
+                             VMGlobalFuncTable &global_funcs) {
+  std::size_t pos = 0;
+  // check buffer size
+  if (buffer.size() < kMinFileSize) return -1;
+  // check file header
+  auto magic_num = IntPtrCast<32>(buffer.data() + pos);
+  if (*magic_num != kFileHeader) return -1;
+  pos += 4;
+  // read symbol table length
+  auto sym_len = IntPtrCast<32>(buffer.data() + pos);
+  pos += 4;
+  // read symbol table
+  std::string symbol;
+  sym_table.clear();
+  for (std::size_t i = 0; i < *sym_len; ++i) {
+    if (buffer[pos + i] == '\0') {
+      sym_table.push_back(symbol);
+      symbol.clear();
+    }
+    else {
+      symbol.push_back(buffer[pos + i]);
+    }
+  }
+  pos += *sym_len;
+  // read global function table length
+  auto global_len = IntPtrCast<32>(buffer.data() + pos);
+  pos += 4;
+  // read global function table
+  VMGlobalFunc glob_func;
+  global_funcs.clear();
+  for (std::size_t i = 0; i < *global_len;) {
+    // read function id
+    auto func_id = IntPtrCast<32>(buffer.data() + pos + i);
+    i += 4;
+    // read function pc
+    glob_func.func_pc = *IntPtrCast<32>(buffer.data() + pos + i);
+    i += 4;
+    // read argument count
+    auto arg_count = buffer[pos + i];
+    i += 1;
+    // read argument id
+    for (std::size_t j = 0; j < arg_count; ++j) {
+      glob_func.args.push_back(*IntPtrCast<32>(buffer.data() + pos + i));
+      i += 4;
+    }
+    // insert to table
+    global_funcs.insert({*func_id, glob_func});
+    // reset
+    glob_func.args.clear();
+  }
+  pos += *global_len;
+  // return position
+  return static_cast<int>(pos);
+}
---
+++ src/vm/codegen.h
@@ -0,0 +1,38 @@
+#ifndef IONIA_VM_BYTECODE_H_
+#define IONIA_VM_BYTECODE_H_
+
+#include <vector>
+#include <map>
+#include <string>
+#include <cstdint>
+
+#include "vm/define.h"
+
+class VMCodeGen {
+ public:
+  VMCodeGen() {}
+
+  // Function returns start position of bytecode segment.
+  // If error, returns -1.
+  static int ParseBytecode(const std::vector<std::uint8_t> &buffer,
+                           VMSymbolTable &sym_table,
+                           VMGlobalFuncTable &global_funcs);
+  std::vector<std::uint8_t> GenerateBytecode();
+  void GenerateBytecodeFile(const std::string &file);
+
+  // getters
+  const VMSymbolTable &sym_table() const { return sym_table_; }
+  const VMGlobalFuncTable &global_funcs() const { return global_funcs_; }
+
+ private:
+  // file header of Ionia VM's bytecode file (bad bite c -> bad byte code)
+  static const std::uint32_t kFileHeader = 0xbadb17ec;
+  // minimum bytecode file size
+  static const std::uint32_t kMinFileSize = 3 * 4;
+
+  // tables
+  VMSymbolTable sym_table_;
+  VMGlobalFuncTable global_funcs_;
+};
+
+#endif  // IONIA_VM_BYTECODE_H_
