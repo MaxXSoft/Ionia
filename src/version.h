befd9bf3f746e53739715510994baf6bb51370f2
MaxXSoft <x@MaxXSoft.net>
2019-05-16 19:56:36 +0800

    (version) added header 'version.h'

---
+++ src/version.h
@@ -0,0 +1,26 @@
+#ifndef IONIA_VERSION_H_
+#define IONIA_VERSION_H_
+
+// version information of Ionia
+
+#ifndef APP_NAME
+#define APP_NAME            "Ionia"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif  // IONIA_VERSION_H_
