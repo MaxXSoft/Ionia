0b4a30c25ab2bedf61343d478466a3434190de93
MaxXSoft <x@MaxXSoft.net>
2019-05-16 15:41:01 +0800

    (back/compiler) completed class 'Compiler'

---
+++ src/back/compiler/compiler.cpp
@@ -0,0 +1,82 @@
+#include "back/compiler/compiler.h"
+
+std::string Compiler::GetNextLabel() {
+  return ":func-" + std::to_string(label_id_++);
+}
+
+void Compiler::GenerateAllFuncDefs() {
+  for (const auto &it : func_defs_) {
+    // generate label
+    gen_.LABEL(it.label);
+    // generate prologue
+    for (const auto &i : it.args) {
+      gen_.POP();
+      gen_.SET(i);
+    }
+    // generate body
+    it.expr->Compile(*this);
+    // generate return
+    gen_.GenReturn();
+  }
+  func_defs_.clear();
+}
+
+std::vector<std::uint8_t> Compiler::GenerateBytecode() {
+  // generate RET instruction
+  gen_.RET();
+  // generate all function definitions
+  GenerateAllFuncDefs();
+  // generate bytecode
+  return gen_.GenerateBytecode();
+}
+
+void Compiler::GenerateBytecodeFile(const std::string &file) {
+  // generate RET instruction
+  gen_.RET();
+  // generate all function definitions
+  GenerateAllFuncDefs();
+  // generate bytecode
+  gen_.GenerateBytecodeFile(file);
+}
+
+void Compiler::Reset() {
+  gen_.Reset();
+  func_defs_.clear();
+  label_id_ = 0;
+}
+
+void Compiler::CompileNext(const ASTPtr &ast) {
+  ast->Compile(*this);
+}
+
+void Compiler::CompileId(const std::string &id) {
+  gen_.GET(id);
+}
+
+void Compiler::CompileNum(int num) {
+  gen_.SetConst(num);
+}
+
+void Compiler::CompileDefine(const std::string &id, const ASTPtr &expr) {
+  expr->Compile(*this);
+  gen_.SET(id);
+}
+
+void Compiler::CompileFunc(const IdList &args, const ASTPtr &expr) {
+  auto label = GetNextLabel();
+  // record function definition
+  func_defs_.emplace_back(FuncDefInfo({label, args, expr->Clone()}));
+  // generate function value
+  gen_.GetFuncValue(label);
+}
+
+void Compiler::CompileFunCall(const std::string &id,
+                              const ASTPtrList &args) {
+  // push all arguments
+  for (auto it = args.rbegin(); it != args.rend(); ++it) {
+    (*it)->Compile(*this);
+    gen_.PUSH();
+  }
+  // call function id
+  gen_.CALL(id);
+}
---
+++ src/back/compiler/compiler.h
@@ -0,0 +1,49 @@
+#ifndef IONIA_BACK_COMPILER_COMPILER_H_
+#define IONIA_BACK_COMPILER_COMPILER_H_
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+#include "define/ast.h"
+#include "vm/codegen.h"
+
+class Compiler {
+ public:
+  Compiler() { Reset(); }
+
+  // generate bytecode buffer
+  std::vector<std::uint8_t> GenerateBytecode();
+  // generate bytecode file
+  void GenerateBytecodeFile(const std::string &file);
+  // reset compiler
+  void Reset();
+
+  // compile next AST
+  void CompileNext(const ASTPtr &ast);
+
+  // visitor functions
+  void CompileId(const std::string &id);
+  void CompileNum(int num);
+  void CompileDefine(const std::string &id, const ASTPtr &expr);
+  void CompileFunc(const IdList &args, const ASTPtr &expr);
+  void CompileFunCall(const std::string &id, const ASTPtrList &args);
+
+ private:
+  struct FuncDefInfo {
+    std::string label;
+    IdList args;
+    ASTPtr expr;
+  };
+
+  // return next label for function generation
+  std::string GetNextLabel();
+  // generate all of function definitions in 'func_defs_'
+  void GenerateAllFuncDefs();
+
+  VMCodeGen gen_;
+  std::vector<FuncDefInfo> func_defs_;
+  int label_id_;
+};
+
+#endif  // IONIA_BACK_COMPILER_COMPILER_H_
